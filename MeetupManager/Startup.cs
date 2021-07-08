6791ace88e7982a544b2126908c8fe1563f0aa72
progr <progr@ARSAL-PC>
2021-07-08 20:53:01 +0500

    Add project files.

---
+++ MeetupManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetupManager.Startup))]
+namespace MeetupManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
